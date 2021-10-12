46c168e2590aed3e82fbe2e40c7449c28c5dcfd6
gitdagray <DaveOnEleven@gmail.com>
2021-10-11 21:56:42 -0500

    first commit

---
+++ middleware/verifyJWT.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const verifyJWT = (req, res, next) => {
+    const authHeader = req.headers.authorization || req.headers.Authorization;
+    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    const token = authHeader.split(' ')[1];
+    jwt.verify(
+        token,
+        process.env.ACCESS_TOKEN_SECRET,
+        (err, decoded) => {
+            //console.log(decoded);
+            if (err) return res.sendStatus(403); //invalid token
+            req.user = decoded.UserInfo.username;
+            req.roles = decoded.UserInfo.roles; //array of nums
+            next();
+        }
+    );
+}
+
+module.exports = verifyJWT
